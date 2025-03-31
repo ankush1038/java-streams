b4b163fc1c03d37a5d7d754adbee8a194e716fe9
ankush1038 <ankusharma1029@gmail.com>
2025-03-31 22:34:40 +0530

    [Ankush] Add: Piped Stream created

---
+++ src/main/java/com/bridgelabz/streams/PipedStream/PipedStream.java
@@ -0,0 +1,65 @@
+package com.bridgelabz.streams.PipedStream;
+
+import java.io.*;
+
+class WriterThread extends Thread {
+    private PipedOutputStream pos;
+
+    public WriterThread(PipedOutputStream pos) {
+        this.pos = pos;
+    }
+
+    @Override
+    public void run() {
+        try {
+            String message = "Hello from WriterThread!";
+            pos.write(message.getBytes());
+            pos.close();
+        } catch (IOException e) {
+            System.err.println("WriterThread Error: " + e.getMessage());
+        }
+    }
+}
+
+class ReaderThread extends Thread {
+    private PipedInputStream pis;
+
+    public ReaderThread(PipedInputStream pis) {
+        this.pis = pis;
+    }
+
+    @Override
+    public void run() {
+        try {
+            int data;
+            while ((data = pis.read()) != -1) {
+                System.out.print((char) data);
+            }
+            pis.close();
+        } catch (IOException e) {
+            System.err.println("ReaderThread Error: " + e.getMessage());
+        }
+    }
+}
+
+public class PipedStream {
+    public static void main(String[] args) {
+        try {
+            PipedOutputStream pos = new PipedOutputStream();
+            PipedInputStream pis = new PipedInputStream(pos);
+
+            WriterThread writer = new WriterThread(pos);
+            ReaderThread reader = new ReaderThread(pis);
+
+            writer.start();
+            reader.start();
+
+            writer.join();
+            reader.join();
+
+        } catch (IOException | InterruptedException e) {
+            System.err.println("Error: " + e.getMessage());
+        }
+    }
+}
+
